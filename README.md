35fa7a273018d20c51ceca201b99ec0bf6fe97c2
lian_liu <lian.liu@oracle.com>
2021-09-15 20:16:22 -0400

    Remove web

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # mavenversion
 
 mvn -Drevision=1.1.1 clean package
+
+testmaven2
+---core
+---client
--- web/src/main/java/com/alvin/testmaven/web/TestWeb.java
+++
@@ -1,9 +0,0 @@
-package com.alvin.testmaven.web;
-
-import com.tangosol.net.NamedCache;
-
-public class TestWeb {
-    public void test(){
-        NamedCache nc;
-    }
-}
